6a8543f8c7facb030b7e9c176c3ddbc69f19db65
Sydney.Hopson <Karatesyd@icloud.com>
2025-05-18 08:44:44 -0400

    updae favicon

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>Sydney Hopson | Full-Stack Developer</title>
+        <meta
+          name="description"
+          content="Portfolio of Sydney L. Hopson Jr., full-stack web developer & systems architect."
+        />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </>
+    );
+  }
+  
